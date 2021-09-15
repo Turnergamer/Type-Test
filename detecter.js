091ad5ec390701ef97a3160e7a62e4b751f9b289
Turnergamer <63259043+Turnergamer@users.noreply.github.com>
2021-09-15 21:01:08 +0100

    Add files via upload

---
+++ detecter.js
@@ -0,0 +1,16 @@
+setTimeout(function(){$("body").keypress(function(key) {
+    if(key.key == " "){
+            if(currentSlideNumber == 0){
+                    location.href = "selection.html"
+            
+            }
+
+        
+    }
+});},500)
+
+
+
+
+
+var currentSlideNumber = 0;
